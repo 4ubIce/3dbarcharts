40a54f1d6e29896b0e35e6a21bee738ed6c27645
Mikhail B <wproxym@at.gmail>
2019-09-11 15:14:42 +0300

    3d bar chart webgl kk: initial

---
+++ main.js
@@ -0,0 +1,250 @@
+﻿var gl;
+var cubeVertexPositionBuffer;
+var cubeVertexColorBuffer;
+var cubeVertexIndexBuffer;
+var mvMatrix = mat4.create();
+var pMatrix = mat4.create();
+var mvMatrixStack = [];
+var shaderProgram;
+var rCube = 0;
+var lastTime = 0;
+var barWidth = 0.2;
+var barPadding = 0.1;
+var start = -1.5;
+var step = 1.0;
+var data = 
+    [
+      {Attr: "a1", Value: 12},
+      {Attr: "a2", Value: 3},
+      {Attr: "a3", Value: 8},
+      {Attr: "a4", Value: 15}
+    ];
+var yScale = d3.scaleLinear()
+               .domain([0, d3.max(data, function(d) {return d.Value})])
+               .range([-1, 1]);    
+    
+
+function webGLStart() {
+  var canvas = document.getElementById("barChart");
+  initGL(canvas);
+  initShaders(); 
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  for (var i = 0; i < data.length; i++) {
+    
+    initBuffers(data[i].Value);
+    tick();
+    start = start + step;
+  }  
+
+}
+
+function initGL(canvas) {
+  try {
+    gl = canvas.getContext("webgl");
+    gl.viewportWidth = canvas.width;
+    gl.viewportHeight = canvas.height;
+  } catch(e) {
+  }
+  if (!gl) {
+    alert("Could not initialise WebGL, sorry :-(");
+  }
+}
+
+function initShaders() {
+  var fragmentShader = getShader(gl, "shader-fs");
+  var vertexShader = getShader(gl, "shader-vs");
+
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert("Could not initialise shaders");
+  }
+
+  gl.useProgram(shaderProgram);
+  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+  shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+  gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+  
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+}
+
+function initBuffers(y) {
+
+  cubeVertexPositionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+  vertices = [
+    // Front face
+    -barWidth, yScale(0),  1.0,
+     barWidth, yScale(0),  1.0,
+     barWidth, yScale(y),  1.0,
+    -barWidth, yScale(y),  1.0,
+
+    // Back face
+    -barWidth, yScale(0), -1.0,
+    -barWidth, yScale(y), -1.0,
+     barWidth, yScale(y), -1.0,
+     barWidth, yScale(0), -1.0,
+
+    // Top face
+    -barWidth, yScale(y), -1.0,
+    -barWidth, yScale(y),  1.0,
+     barWidth, yScale(y),  1.0,
+     barWidth, yScale(y), -1.0,
+
+    // Bottom face
+    -barWidth, yScale(0), -1.0,
+     barWidth, yScale(0), -1.0,
+     barWidth, yScale(0),  1.0,
+    -barWidth, yScale(0),  1.0,
+
+    // Right face
+     barWidth, yScale(0), -1.0,
+     barWidth, yScale(y), -1.0,
+     barWidth, yScale(y),  1.0,
+     barWidth, yScale(0),  1.0,
+
+    // Left face
+    -barWidth, yScale(0), -1.0,
+    -barWidth, yScale(0),  1.0,
+    -barWidth, yScale(y),  1.0,
+    -barWidth, yScale(y), -1.0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  cubeVertexPositionBuffer.itemSize = 3;
+  cubeVertexPositionBuffer.numItems = 24;
+  
+  cubeVertexColorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexColorBuffer);
+  colors = [
+    [1.0, 0.0, 0.0, 1.0],     // Front face
+    [1.0, 1.0, 0.0, 1.0],     // Back face
+    [0.0, 1.0, 0.0, 1.0],     // Top face
+    [1.0, 0.5, 0.5, 1.0],     // Bottom face
+    [1.0, 0.0, 1.0, 1.0],     // Right face
+    [0.0, 0.0, 1.0, 1.0],     // Left face
+  ];
+  var unpackedColors = [];
+  for (var i in colors) {
+    var color = colors[i];
+    for (var j=0; j < 4; j++) {
+      unpackedColors = unpackedColors.concat(color);
+    }
+  }
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(unpackedColors), gl.STATIC_DRAW);
+  cubeVertexColorBuffer.itemSize = 4;
+  cubeVertexColorBuffer.numItems = 24;
+  
+  cubeVertexIndexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+  var cubeVertexIndices = [
+    0, 1, 2,      0, 2, 3,    // Front face
+    4, 5, 6,      4, 6, 7,    // Back face
+    8, 9, 10,     8, 10, 11,  // Top face
+    12, 13, 14,   12, 14, 15, // Bottom face
+    16, 17, 18,   16, 18, 19, // Right face
+    20, 21, 22,   20, 22, 23  // Left face
+  ]
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+  cubeVertexIndexBuffer.itemSize = 1;
+  cubeVertexIndexBuffer.numItems = 36;    
+}
+
+function tick() {
+  //requestAnimFrame(tick);
+  drawScene();
+  //animate();
+}
+
+function drawScene() {
+  //gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  //gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+  mat4.identity(mvMatrix);
+  
+  mat4.translate(mvMatrix, [start, 0.0, -7.0]);
+  //mvPushMatrix();
+  mat4.rotate(mvMatrix, degToRad(rCube), [0, 1, 0]);  
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexColorBuffer);
+  gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, cubeVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+  setMatrixUniforms();
+  gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+  
+  //mvPopMatrix();
+}
+
+function getShader(gl, id) {
+    var shaderScript = document.getElementById(id);
+    if (!shaderScript) {
+        return null;
+    }
+
+    var str = "";
+    var k = shaderScript.firstChild;
+    while (k) {
+        if (k.nodeType == 3)
+            str += k.textContent;
+        k = k.nextSibling;
+    }
+
+    var shader;
+    if (shaderScript.type == "x-shader/x-fragment") {
+        shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+        shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+        return null;
+    }
+
+    gl.shaderSource(shader, str);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert(gl.getShaderInfoLog(shader));
+        return null;
+    }
+
+    return shader;
+}
+
+function degToRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function setMatrixUniforms() {
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+  gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+function animate() {
+  var timeNow = new Date().getTime();
+  if (lastTime != 0) {
+    var elapsed = timeNow - lastTime;
+
+    rCube -= (75 * elapsed) / 1000.0;
+  }
+  lastTime = timeNow;
+} 
+
+function mvPushMatrix() {
+  var copy = mat4.create();
+  mat4.set(mvMatrix, copy);
+  mvMatrixStack.push(copy);
+}
+
+function mvPopMatrix() {
+  if (mvMatrixStack.length == 0) {
+    throw "Invalid popMatrix!";
+  }
+  mvMatrix = mvMatrixStack.pop();
+} 
