9bae2f8be361745d922bee714425eb66c1a05aba
Kirill Kupriyanov <humanidx@tut.by>
2019-10-18 11:53:43 +0300

    some refactoring v3

--- script/global/Buffers.js
+++ script/global/Buffers.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 class Buffers {
 
     constructor(gl, sp) {
--- script/global/ShaderProgramm.js
+++ script/global/ShaderProgramm.js
@@ -1,9 +1,9 @@
-'use strict';
+﻿'use strict';
 class ShaderProgramm {
 
     constructor(element) {
         this.gl = this.initGL(element);
-        this.shaderProgram = this.initShaders(this.gl);
+        this.shaderProgram = this.initShaderProgram(this.gl);
     }
     
     initGL(canvas) {
@@ -27,10 +27,11 @@ class ShaderProgramm {
         return gl;
     }
 
-    initShaders(gl) { 
-    
-        let fragmentShader = this.getShader('shader-fs', gl);
-        let vertexShader = this.getShader('shader-vs', gl);
+    initShaderProgram(gl) { 
+                           
+        let shaders = new Shaders();
+        let fragmentShader = shaders.getShader('shader-fs', gl);
+        let vertexShader = shaders.getShader('shader-vs', gl);
         let shaderProgram = gl.createProgram();
         
         gl.attachShader(shaderProgram, vertexShader);
@@ -64,74 +65,4 @@ class ShaderProgramm {
         
         return shaderProgram;               
     } 
-
-    getShader(id, gl) {
-        let shaderScript;
-        let shader;
-        if (id == 'shader-fs') {
-            shader = gl.createShader(gl.FRAGMENT_SHADER);
-            shaderScript = this.getFShaderScript();
-        } else if (id == 'shader-vs') {
-            shader = gl.createShader(gl.VERTEX_SHADER);
-            shaderScript = this.getVShaderScript();
-        } else {
-            return null;
-        }
-
-        gl.shaderSource(shader, shaderScript);
-        gl.compileShader(shader);
-
-        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-            alert(gl.getShaderInfoLog(shader));
-            return null;
-        }
-
-        return shader;
-    } 
-        
-    getFShaderScript() {
-        let script = `precision mediump float;
-      
-                      varying vec2 vTextureCoord;
-                      varying vec4 vColor;
-                      varying vec3 vLightWeighting;
-                        
-                      uniform vec4 vColor1;
-                      uniform vec4 vColor2;
-                      uniform sampler2D uSampler;
-
-                      void main(void) {
-                          vec4 textureColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t)) * vColor1 + vColor * vColor2;
-                          gl_FragColor = vec4(textureColor.rgb * vLightWeighting, textureColor.a);                          
-                      }`;
-        return script;    
-    }
-    
-    getVShaderScript() {
-        let script = `attribute vec3 aVertexPosition;
-                      attribute vec3 aVertexNormal;
-                      attribute vec4 aVertexColor;
-                      attribute vec2 aTextureCoord;
-
-                      uniform mat4 uMVMatrix;
-                      uniform mat4 uPMatrix;
-                      uniform mat3 uNMatrix;
-                      uniform vec3 uAmbientColor;
-                      uniform vec3 uLightingDirection;
-                      uniform vec3 uDirectionalColor;
-                      varying vec4 vColor;
-                      varying vec2 vTextureCoord;
-                      varying vec3 vLightWeighting;                      
-
-
-                      void main(void) {
-                          gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
-                          vTextureCoord = aTextureCoord;
-                          vec3 transformedNormal = uNMatrix * aVertexNormal;
-                          float directionalLightWeighting = max(dot(transformedNormal, uLightingDirection), 0.0);
-                          vLightWeighting = uAmbientColor + uDirectionalColor * directionalLightWeighting;
-                          vColor = aVertexColor * vec4(vLightWeighting, 1.0);                          
-                      }`;
-        return script;    
-    }    
 }
