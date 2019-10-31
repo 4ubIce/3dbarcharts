6f98b2a845123353886be86a4501a4f293526a15
Kirill Kupriyanov <humanidx@tut.by>
2019-10-31 15:46:32 +0300

    add jasmine js browser test

--- test/test.js
+++ test/test.js
@@ -2,8 +2,10 @@
 
 require('amd-loader');
 
-let assert = require('assert');
-let requirejs = require('requirejs');
+const assert = require('assert');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const requirejs = require('requirejs');
 requirejs.config({
     baseUrl: 'script',
     paths: {
@@ -22,7 +24,6 @@ requirejs.config({
         ClassHelper: './helpers/ClassHelper'
     }
 });
-
 
 describe('MatrixStack', function() {
 
@@ -53,4 +54,39 @@ describe('MatrixStack', function() {
             assert.equal(m.length, 16);
         });
     });        
+});
+
+describe('ThreeDBarChart', function() {
+
+    const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+    console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+/*
+    let ThreeDBarChart = requirejs('ThreeDBarChart');
+    let barChart = new ThreeDBarChart();
+
+    describe('new MatrixStack()', function() {
+        it('should contain empty array', function() {
+          assert.equal(stack.matrixStack.length, 0);
+        });
+    });
+    
+    describe('MatrixStack().push(matrix)', function() {
+
+        let mat = require('gl-Matrix');
+        let m = mat.mat4.create();
+
+        it('should push matrix into array', function() {
+            stack.push(m);
+            assert(stack.matrixStack.length > 0);
+        });
+    });
+    
+    describe('MatrixStack().pop()', function() {
+        it('should return matrix and remove element from array', function() {
+            let m = stack.pop();
+            assert.equal(stack.matrixStack.length, 0);
+            assert.equal(m.length, 16);
+        });
+    });
+*/            
 });
